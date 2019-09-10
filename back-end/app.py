ffea7141c594b37d385e56e8f0969d431f9df179
yue <1995yuexiao@gmail.com>
2019-09-10 22:55:33 +0100

    update pictures

--- back-end/app.py
+++ back-end/app.py
@@ -7,6 +7,22 @@ app = Flask(__name__)
 def hello_world():
     return render_template("/home.html")
 
+@app.route('/resume')
+def hello_world():
+    return render_template("/resume.html")
+
+@app.route('/projects')
+def hello_world():
+    return render_template("/projects.html")
+
+@app.route('/blogs')
+def hello_world():
+    return render_template("/blogs.html")
+
+@app.route('/about-me')
+def hello_world():
+    return render_template("/about-me.html")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug='True')
