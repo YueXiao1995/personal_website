00361f63046fb5cba44a9e185bf91a75ec63574f
yue <1995yuexiao@gmail.com>
2019-09-12 16:56:46 +0100

    maintenance page

--- back-end/app.py
+++ back-end/app.py
@@ -2,27 +2,28 @@ from flask import Flask, render_template, request, make_response, redirect, url_
 
 app = Flask(__name__)
 
+is_under_maintenance = False
 
 @app.route('/')
-def hello_world():
-    return render_template("/home.html")
+def home():
+    return render_template("/maintenance.html")
+    #return render_template("/home.html")
 
 @app.route('/resume')
-def hello_world():
+def resume():
     return render_template("/resume.html")
 
 @app.route('/projects')
-def hello_world():
+def projects():
     return render_template("/projects.html")
 
 @app.route('/blogs')
-def hello_world():
+def blogs():
     return render_template("/blogs.html")
 
 @app.route('/about-me')
-def hello_world():
+def about_me():
     return render_template("/about-me.html")
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug='True')
